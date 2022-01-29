5d7c219bac91445fcb64b3b048afeebf8d8b65a0
debiprasan singh <singhprasandebi123@gmail.com>
2022-01-30 03:08:20 +0530

    add new github code

---
+++ selenium_class/src/pack2/GithubDemopush.java
@@ -0,0 +1,11 @@
+package pack2;
+
+public class GithubDemopush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("trying with GIT");
+	}
+
+}
