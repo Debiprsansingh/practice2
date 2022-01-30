9c6d04b3c8b52a295a64b42703cfac568e555a72
debiprasan singh <singhprasandebi123@gmail.com>
2022-01-31 02:06:31 +0530

    Revert "adding here the 2nd line"
    This reverts commit 3a149b5159b3d707791fe0daa342acf23a7e1a31.

--- durgasoft/src/com/git/Git_test1.java
+++ durgasoft/src/com/git/Git_test1.java
@@ -6,7 +6,6 @@ public class Git_test1 {
 		// TODO Auto-generated method stub
 
 		System.out.println("i am adding here the first line");
-		System.out.println("i am adding here the 2nd line");
 		
 	}
 
